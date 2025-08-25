67014bedde2c8005cade88bab19c54dc3b46d0ff
Tishan Yasiru <Tishanyasiru@outlook.com>
2025-08-25 12:29:58 +0530

    fix : .env error

--- Fast-Api/agents/web_search.py
+++ Fast-Api/agents/web_search.py
@@ -1,8 +1,12 @@
 from typing import List, Dict, Any
 import os, requests
 
-GOOGLE_API_KEY = 'AIzaSyDjUlbxqwB320cqOB1GO5NKGGuer2wW71s'
-GOOGLE_CSE_ID = 'e4bde7703038b46f3'
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_CSE_ID = os.getenv("GOOGLE_CSE_ID")
+
+
+# GOOGLE_API_KEY ='AIzaSyCEzNUMQe7OAZXt6K_U0bSs8Rl_T4Rqpq0'
+# GOOGLE_CSE_ID ='136302d29adba4a18'
 
 def web_search_sync(query: str, max_results: int = 5) -> List[Dict[str, Any]]:
     if not GOOGLE_API_KEY or not GOOGLE_CSE_ID:

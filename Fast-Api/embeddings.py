9fe48bbe33bce75bd3dfd5284839f583572d1339
vgLashika <vglashikasathruwan@gmail.com>
2025-10-16 14:37:32 +0530

    update main.py for support pdf_quiring

--- Fast-Api/embeddings.py
+++ Fast-Api/embeddings.py
@@ -1,24 +1,24 @@
-# import threading
-# from sentence_transformers import SentenceTransformer
+import threading
+from sentence_transformers import SentenceTransformer
 
-# _model = None
-# _lock = threading.Lock()
+_model = None
+_lock = threading.Lock()
 
-# def get_embedding_model():
-#     global _model
-#     if _model is None:
-#         with _lock:
-#             if _model is None:
-#                 _model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-#     return _model
+def get_embedding_model():
+    global _model
+    if _model is None:
+        with _lock:
+            if _model is None:
+                _model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    return _model
 
-# def embed_text(text: str):
-#     model = get_embedding_model()
-#     return model.encode([text])[0]
+def embed_text(text: str):
+    model = get_embedding_model()
+    return model.encode([text])[0]
 
 
 
-#---------------------------------------------------------------
+# ---------------------------------------------------------------
 
 from sentence_transformers import SentenceTransformer
 import numpy as np
